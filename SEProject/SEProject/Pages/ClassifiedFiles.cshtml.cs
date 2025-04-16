ef9d35e5fd4c28e17eff482b307a3906b47abf5c
Anghel-Marcel <anghel.marcel.t8t@student.ucv.ro>
2025-04-16 21:30:19 +0300

    Add Navigation Bar + Pages

---
+++ SEProject/SEProject/Pages/ClassifiedFiles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEProject.Pages
+{
+    public class ClassifiedFilesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SEProject/SEProject/Pages/Conference.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEProject.Pages
+{
+    public class ConferenceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
